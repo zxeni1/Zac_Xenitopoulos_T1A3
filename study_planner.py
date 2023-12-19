c0e114e484907963411d14d3111d8f86641ff5ee
zxeni1 <zacxfitness@gmail.com>
2023-12-19 16:40:43 +1000

    Added JSON file load_data

--- study_planner.py
+++ study_planner.py
@@ -1,2 +1,10 @@
 import json
 from datetime import datetime, timedelta
+
+def load_data():
+    try:
+        with open("study_planner.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        data = {"classes": [], "assignments": [], "exams": []}
+    return data
