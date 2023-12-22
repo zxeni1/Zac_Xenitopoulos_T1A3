576b3d78f3dd97b224cbaa8946b70190665b9203
zxeni1 <zacxfitness@gmail.com>
2023-12-22 15:48:07 +1000

    Added run.sh, packages and code troubleshoot

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Check if python3 is installed
+if command -v python3 &>/dev/null; then
+    echo "Python 3 is installed."
+    
+    # Check if python3 needs to be updated
+    if python3 -c 'import sys; exit(sys.version_info < (3, 9))'; then
+        echo "Python 3 is up to date."
+    else
+        echo "Updating Python 3..."
+        # Update Python 3
+        # Note: The specific command may vary based on the package manager used on your system (e.g., apt, yum, brew).
+        # The following is an example for systems using apt.
+        sudo apt-get update
+        sudo apt-get install --only-upgrade python3
+    fi
+else
+    # Prompt user to install python3
+    echo "Python 3 is not installed. Please install Python 3 before proceeding."
+    # You can provide instructions or links to install Python 3 based on the user's operating system.
+    # For example, on Ubuntu:
+    echo "On Ubuntu, you can install Python 3 using: sudo apt-get install python3"
+    exit 1
+fi
+
+# Create a virtual environment
+echo "Creating virtual environment..."
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the specified packages
+pip install altgraph==0.17.2 colored==2.2.3 future==0.18.2 macholib==1.15.2 numpy==1.26.2 pandas==2.1.4 prettytable==3.9.0 python-dateutil==2.8.2 pytz==2023.3.post1 six==1.15.0 tzdata==2023.3 wcwidth==0.2.12
+
+# Run your Python script
+python3 study_planner.py
+
+# Deactivate the virtual environment
+deactivate
--- study_planner.py
+++ study_planner.py
@@ -1,28 +1,45 @@
-# Packages
 from colored import fg, attr, bg
 import pandas as pd
 from datetime import datetime
+from prettytable import PrettyTable
+
+# Function to save data to JSON file using pandas
+def save_data(data):
+    print("Data to be saved:")
+    print(data)
+    data.to_json("study_planner.json", orient="records", date_format="iso", indent=2)
+    
+    # Load the data again to ensure changes are applied
+    return load_data()
 
 # Function to load data from JSON file using pandas
 def load_data():
     try:
-        data = pd.read_json("study_planner.json")
+        data = pd.read_json("study_planner.json", convert_dates=["due_date"])
     except FileNotFoundError:
         data = pd.DataFrame(columns=["name", "due_date", "type"])
+    else:
+        # Add 'type' column if it doesn't exist
+        if 'type' not in data.columns:
+            data['type'] = ""
+    print("Loaded Data:")
+    print(data)
     return data
-
-# Function to save data to JSON file using pandas
-def save_data(data):
-    data.to_json("study_planner.json", orient="records", date_format="iso", indent=2)
 
 # Function to add a new task such as class, assignment, or exam
 def add_task(data, task_type):
     name = get_user_input("Enter the name of the {}: ".format(task_type))
     due_date = get_valid_date_input("Enter the due date (DD/MM/YYYY): ")
 
-    task = pd.DataFrame({"name": [name], "due_date": [due_date], "type": [task_type]})
+    # Format the due date to "DD/MM/YYYY" before saving
+    due_date_str = due_date.strftime("%d/%m/%Y")
+
+    task = pd.DataFrame({"name": [name], "due_date": [due_date_str], "type": [task_type]})
     data = pd.concat([data, task], ignore_index=True)
-    save_data(data)
+    
+    # Save the data immediately and load it back
+    data = save_data(data)
+    
     print("{} added successfully!".format(task_type.capitalize()))
 
 # Function to edit a task such as class, assignment, or exam
@@ -56,8 +73,11 @@ def delete_task(data, task_type):
     print("List of {}: {}".format(task_type, data[data["type"] == task_type]["name"].tolist()))
     task_name = input("Enter the name of the {} to delete: ".format(task_type))
 
+    # Convert task name to string and strip leading and trailing whitespaces, and make the comparison case-insensitive
+    task_name = str(task_name).strip().lower()
+
     for index, task in data[data["type"] == task_type].iterrows():
-        if task["name"] == task_name:
+        if str(task["name"]).strip().lower() == task_name:
             data = data.drop(index, axis=0).reset_index(drop=True)
             save_data(data)
             print("{} deleted successfully!".format(task_type.capitalize()))
@@ -65,38 +85,21 @@ def delete_task(data, task_type):
 
     print("{} not found!".format(task_type.capitalize()))
 
-# Function to check reminders for classes
-def check_class_reminders(data):
-    today = datetime.today().date()
-    upcoming_classes = data[(data["type"] == "classes") & (data["due_date"] > today)]
-
-    if not upcoming_classes.empty:
-        print("\nUpcoming Classes:")
-        for _, task in upcoming_classes.iterrows():
-            print("{} - On {}".format(task["name"], task["due_date"]))
-    else:
-        print("\nNo upcoming classes!")
-
-# Function to check reminders for assignments and exams
-def check_exam_assignment_reminders(data):
-    today = datetime.today().date()
-    upcoming_tasks = data[(data["type"].isin(["assignments", "exams"])) & (data["due_date"] > today)]
-
-    if not upcoming_tasks.empty:
-        print("\nUpcoming Exams and Assignments:")
-        for _, task in upcoming_tasks.iterrows():
-            print("{} ({}) - Due on {}".format(task["name"], task["type"].capitalize(), task["due_date"]))
-    else:
-        print("\nNo upcoming exams or assignments!")
-
 # Function to check all tasks
 def check_all_tasks(data):
-    print("\nAll Tasks:")
-    for task_type in ["classes", "assignments", "exams"]:
-        task_subset = data[data["type"] == task_type]
-        print("\n{}:".format(task_type.capitalize()))
-        for _, task in task_subset.iterrows():
-            print("{} - Due on {}".format(task["name"], task["due_date"]))
+    today = datetime.today().date()
+
+    if not data.empty and 'due_date' in data.columns:
+        print("\nAll Tasks:")
+        task_subset = data[pd.to_datetime(data["due_date"]).dt.date > today]
+
+        if not task_subset.empty:
+            for _, task in task_subset.iterrows():
+                print(f"{task['name']} ({task['type'].capitalize()}) - Due on {task['due_date'].strftime('%d/%m/%Y')}")
+        else:
+            print("No upcoming tasks!")
+    else:
+        print("There is nothing to check!")
 
 # Function to get user input
 def get_user_input(prompt):
@@ -114,7 +117,7 @@ def get_valid_date_input(prompt):
 # Main function
 def main():
     data = load_data()
-
+    
     while True:
         print(f"{fg('white')}{bg('black')}\nStudy Planner Menu{attr('reset')}")
         print(f"{fg('blue')}{bg('black')}1. Add Class")
@@ -126,12 +129,10 @@ def main():
         print("7. Delete Class")
         print("8. Delete Assignment")
         print("9. Delete Exam")
-        print("10. Check Class Reminders")
-        print("11. Check Exam and Assignment Reminders")
-        print("12. Check All")
+        print("10. Check All Tasks")
         print(f"{fg('white')}{bg('black')}0. Exit")
 
-        choice = get_user_input("Enter your choice (0-12): ")
+        choice = get_user_input("Enter your choice (0-10): ")
 
         if choice == "0":
             break
@@ -154,13 +155,11 @@ def main():
         elif choice == "9":
             delete_task(data, "exams")
         elif choice == "10":
-            check_class_reminders(data)
-        elif choice == "11":
-            check_exam_assignment_reminders(data)
-        elif choice == "12":
             check_all_tasks(data)
         else:
-            print("Invalid choice. Please enter a number between 0 and 12.")
+            print("Invalid choice. Please enter a number between 0 and 10.")
+        
+        data = load_data()
 
 if __name__ == "__main__":
     main()
